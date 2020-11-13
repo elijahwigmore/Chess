829cdec7516bd14a2d30f5cb39d77bbbf96302c7
Elijah Wigmore <elijah.wigmore@mail.utoronto.ca>
2020-11-12 16:09:31 -0800

    Added algorithm for taking opponent's pieces by value if able to.

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ var board = Chessboard('myBoard')
 var game = null
 var algorithmMethod = null
 
+var pieceValues = {
+  'p': 10,
+  'n': 30,
+  'b': 30,
+  'r': 50,
+  'q': 100,
+  'k': 900
+}
+
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
@@ -32,6 +41,48 @@ function makeFirstMove () {
 
   game.move(possibleMoves[0])
   board.position(game.fen())
+}
+
+function makePositionEvaluationMove () {
+  var possibleMoves = game.moves()
+
+  // game over
+  if (possibleMoves.length === 0) return
+
+  var opponentValue = getPlayerValue('w')
+  var selectedMoveIndex = Math.floor(Math.random() * possibleMoves.length)
+
+  for (var i = 0; i < possibleMoves.length; i++) {
+    // simulate making a move
+    game.move(possibleMoves[i])
+
+    var newOpponentValue = getPlayerValue('w')
+    if (opponentValue > newOpponentValue) {
+      opponentValue = newOpponentValue
+      selectedMoveIndex = i
+    }
+
+    // undo the move
+    game.undo()
+  }
+
+  game.move(possibleMoves[selectedMoveIndex])
+  board.position(game.fen())
+}
+
+function getPlayerValue (color) {
+  var value = 0
+
+  for (var i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++) {
+      var piece = game.board()[i][j]
+      if (piece && piece.color == color) {
+        value += pieceValues[piece.type]
+      }
+    }
+  }
+
+  return value
 }
 
 function onDrop (source, target) {
@@ -69,11 +120,14 @@ function startGame() {
 
   var algorithm = document.getElementById('algorithms').value
   switch (algorithm) {
-    case 'Random':
+    case 'Random Move':
       algorithmMethod = makeRandomMove
       break
-    case 'First':
+    case 'First Move':
       algorithmMethod = makeFirstMove
       break
+      case 'Position Evaluation':
+        algorithmMethod = makePositionEvaluationMove
+        break
   }
 }
